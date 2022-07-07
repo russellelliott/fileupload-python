c2552c1d34a53f937d161dc0e38678b8c0b745d7
Russell Elliott <russell@quadsquad.net>
2022-07-06 21:45:42 -0700

    search implementaiton complete

--- app.py
+++ app.py
@@ -57,12 +57,12 @@ def searchpage():
 from flask_autoindex import AutoIndex
 from flask import send_from_directory
 
-#Searching files for specific keywords
+'''#Searching files for specific keywords
 @app.route('/search', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     processed_text = text.lower() #text from the input converted to lowercase
-    return processed_text
+    return processed_text'''
 
 from flask_moment import Moment
 from datetime import datetime
@@ -80,15 +80,24 @@ def byte_units(value, units=-1):
 app.jinja_env.filters.update(byte_units = byte_units)
 moment = Moment(app)
 
+#Make the upload folder if it doesn't exist already
 try:
     os.makedirs(app.config['UPLOAD_FOLDER'])
 except:
     pass
 
-def get_files(target):
+#Check the file to see if it contains all the keywords specified
+def filter(file, words):
+    f = open(file, "r") #read the file
+    string = f.read().lower() #read file, convert to lowercase
+    res = [ele for ele in words if(ele in string)] #test if all elements in the words list are in the string
+    return res
+
+def get_files(target, words):
     for file in os.listdir(target):
-        path = os.path.join(target, file)
-        if os.path.isfile(path):
+        path = os.path.join(target, file) #path to the file
+        if os.path.isfile(path) and filter(path, words): #check if the path is valid and the file contains all the keywords
+            #yield statement to add file to list displayed
             yield (
                 file,
                 datetime.utcfromtimestamp(os.path.getmtime(path)),
@@ -96,11 +105,17 @@ def get_files(target):
             )
 
 #Viewing the files in the directory
-@app.route('/files')
-def files():
-    files = get_files(app.config['UPLOAD_FOLDER'])
-    return render_template('files.html', **locals(), variable = UPLOAD_FOLDER)
+#This should take the data from search
 
+#Searching files for specific keywords
+@app.route('/search', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    keywords = text.lower().split() #text from the input converted to lowercase
+    files = get_files(app.config['UPLOAD_FOLDER'], keywords) #get files from the uploads folder that contain the keywords
+    return render_template('files.html', **locals(), variable = UPLOAD_FOLDER) #render the list of files, as well as date they were modified, and their size
+
+#Dowload file from list
 @app.route('/download/<path:filename>')
 def download(filename):
     return send_from_directory(
